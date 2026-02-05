561471813c4528e96115ad998bc93953f568f987
谷本優樹 <143520538+TanimotoYuuki@users.noreply.github.com>
2026-02-05 09:53:06 +0900

    遊び方の追加と不具合修正 (#38)
    Co-authored-by: 谷本優樹 <yuki016.kawa@gmail.com>

--- GameData/Game/TitleSprite.h
+++ GameData/Game/TitleSprite.h
@@ -12,6 +12,16 @@ public:
 	bool Start();//開始処理
 	void Render(RenderContext& rc);//描画処理
 
+public://列挙型
+
+	//タイトルで描画するUIの種類
+	enum EnDrawingUI
+	{
+		enDrawingUI_TextUI,//テキストUI
+		enDrawingUI_HowToPlayUI,//遊び方UI
+		enDrawingUI_Num//UIの種類
+	};
+
 private://メンバ関数
 
 	/// <summary>
@@ -24,28 +34,34 @@ private://メンバ関数
 	/// </summary>
 	void InitStartUIText();
 
+	/// <summary>
+	/// 遊び方UIの初期化
+	/// </summary>
+	void InitHowToPlayUI();
+
 public://メンバ関数
 
 	/// <summary>
 	/// UIを描画する
 	/// </summary>
-	void EnableDrawingUI()
+	void EnableDrawingUI(EnDrawingUI drawingUI)
 	{
-		m_isDrawingUI = true;
+		m_isDrawingUI[drawingUI] = true;
 	}
 
 	/// <summary>
 	/// UIを描画しない
 	/// </summary>
-	void DisableDrawingUI()
+	void DisableDrawingUI(EnDrawingUI drawingUI)
 	{
-		m_isDrawingUI = false;
+		m_isDrawingUI[drawingUI] = false;
 	}
 
 private://メンバ変数
 	SpriteRender m_titleTextUI;//タイトルテキストUI
 	SpriteRender m_startTextUI;//スタートテキストUI
-	bool m_isDrawingUI = false;//UIを描画するか?
+	SpriteRender m_howToPlayUI;//遊び方テキストUI
+	bool m_isDrawingUI[enDrawingUI_Num] = { false };//UIを描画するか?
 
 private://表示するUIのファイルパス用のメンバ変数
 
@@ -54,5 +70,8 @@ private://表示するUIのファイルパス用のメンバ変数
 
 	//スタートテキストUIのファイルパス
 	const std::string m_startTextUIFilePath = "Assets/sprite/text/start_abutton.dds";
+
+	//遊び方テキストUIのファイルパス
+	const std::string m_howToPlayUIFilePath = "Assets/sprite/howToPlay/howToPlay.dds";
 };
 
